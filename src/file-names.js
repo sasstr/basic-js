82eeb73f84a457415577d3599fbf36861703ac7b
Alexander Strakhovenko <sasstr@gmail.com>
2023-04-18 00:47:03 +0300

    feat: implement task file names

--- src/file-names.js
+++ src/file-names.js
@@ -15,10 +15,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+function renameFiles(names) {
+  const result = [];
+
+  names.forEach( (name) => {
+    let count = 1;
+
+    if (result.includes(name)) {
+      while (result.includes(`${name}(${count})`)) {
+        count +=1
+      };
+      result.push(`${name}(${count})`);
+    } else {
+      result.push(name);
+    }
+  })
+  return result;
+};
 
 module.exports = {
   renameFiles
